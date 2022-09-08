220313c7a4a258c9ec47876869e186c44b9bde77
daria kozhevnikova <kozhevnikova.d.n@gmail.com>
2022-09-08 14:51:45 +0100

    hello from me

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class HelloWorldTest {
+
+    @Test
+    public void testHelloWorld(){
+       Response response = RestAssured.
+               get("https://playground.learnqa.ru/api/hello")
+               .andReturn();
+       response.prettyPrint();
+    }
 }
