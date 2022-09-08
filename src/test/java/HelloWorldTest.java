56726371769147d91f2f822272f23e0cbd7fdee6
daria kozhevnikova <kozhevnikova.d.n@gmail.com>
2022-09-08 15:08:09 +0100

    Add Get Test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -7,8 +7,8 @@ public class HelloWorldTest {
     @Test
     public void testHelloWorld(){
        Response response = RestAssured.
-               get("https://playground.learnqa.ru/api/hello")
+               get("https://playground.learnqa.ru/api/get_text")
                .andReturn();
-       response.prettyPrint();
+        System.out.println(response.asString());
     }
 }
