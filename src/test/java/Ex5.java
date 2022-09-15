600f1cc78106f1f81780e2b1f3530ba036900a06
daria kozhevnikova <kozhevnikova.d.n@gmail.com>
2022-09-15 10:58:23 +0100

    Add answer for Ex5

---
+++ src/test/java/Ex5.java
@@ -0,0 +1,19 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+public class Ex5 {
+
+    @Test
+    public void testSecondMessage(){
+        JsonPath response = RestAssured
+               .get("https://playground.learnqa.ru/api/get_json_homework")
+               .jsonPath();
+        ArrayList<LinkedHashMap<String,String>> answer = response.get("messages");
+
+        System.out.println(answer.get(1).get("message"));
+    }
+}
--- src/test/java/HelloWorldTestDZ.java
+++ src/test/java/HelloWorldTestDZ.java
@@ -2,10 +2,10 @@ import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
-public class HelloWorldTestDZ {
+public class HelloWorldTest {
 
     @Test
-    public void testHelloWorldDZ(){
+    public void testHelloWorld(){
        Response response = RestAssured.
                get("https://playground.learnqa.ru/api/get_text")
                .andReturn();
