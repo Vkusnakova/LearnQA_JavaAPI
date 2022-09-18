42410db4cb771b755b6904a11112dd59c7874fff
daria kozhevnikova <kozhevnikova.d.n@gmail.com>
2022-09-18 22:25:08 +0100

    Fix Ex7

--- src/test/java/Ex7.java
+++ src/test/java/Ex7.java
@@ -8,10 +8,11 @@ public class Ex7 {
     @Test
     public void testLongRedirect(){
         String url = "https://playground.learnqa.ru/api/long_redirect";
+        System.out.println(url);
         int responseCode = 0;
         int numberOfRedirescts = 0;
 
-        while(responseCode!=200) {
+        while(responseCode!= 200) {
             Response response = RestAssured
                     .given()
                     .redirects()
@@ -21,10 +22,15 @@ public class Ex7 {
                     .andReturn();
             url = response.getHeaders().getValue("Location");
             responseCode = response.getStatusCode();
-            numberOfRedirescts++;
+            if (responseCode == 200){
+                break;
+            }
+            else {
+                System.out.println(url);
+                numberOfRedirescts += 1;
+            }
         }
         System.out.println(numberOfRedirescts);
-
 
     }
 }
