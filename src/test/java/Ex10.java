ee65ddcd5b93de4bfc8df624d8b81629ca1dbfbb
daria kozhevnikova <kozhevnikova.d.n@gmail.com>
2022-09-22 13:53:55 +0300

    Add Ex10

---
+++ src/test/java/Ex10.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Ex10 {
+
+    @Test
+    public void testLenghtOdString(){
+        String s = "alkfjalfgkgl";
+
+            Assertions.assertTrue(s.length()>15, "String lenght is less than 15");
+
+
+    }
+}
