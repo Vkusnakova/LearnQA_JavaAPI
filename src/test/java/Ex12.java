b483448a3072b2c20337306fb32a69f561c01145
daria kozhevnikova <kozhevnikova.d.n@gmail.com>
2022-09-23 14:46:29 +0300

    Add Ex12

---
+++ src/test/java/Ex12.java
@@ -0,0 +1,26 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Ex12 {
+
+    @Test
+    public void testHeaders() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+        Headers headers = response.getHeaders();
+
+
+        assertTrue(headers.hasHeaderWithName("x-secret-homework-header"),
+                "Header with name 'x-secret-homework-header' doesn't exist");
+        Assertions.assertEquals("Some secret value", headers.getValue("x-secret-homework-header"),
+                "Header is different");
+
+    }
+
+}
